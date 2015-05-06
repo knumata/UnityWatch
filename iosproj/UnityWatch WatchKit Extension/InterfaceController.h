cbf7631230f6d6f71b087ef5f666fdfec363f226
沼田 和幸 <kznumata@gmail.com>
2015-05-07 05:06:19 +0900

    add WatchKit

---
+++ iosproj/UnityWatch WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  UnityWatch WatchKit Extension
+//
+//  Created by 沼田 和幸 on 2015/05/07.
+//
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ iosproj/UnityWatch WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  UnityWatch WatchKit Extension
+//
+//  Created by 沼田 和幸 on 2015/05/07.
+//
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
