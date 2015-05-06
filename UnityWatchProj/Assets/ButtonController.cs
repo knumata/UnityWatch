2f9b5730017afe927d8cc9e13c3c0507364fbcaa
沼田 和幸 <kznumata@gmail.com>
2015-05-07 03:14:24 +0900

    add UnitWatchProj (Unity Project)

---
+++ UnityWatchProj/Assets/ButtonController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void changeBallColor () {
+		Debug.Log ("Clicked.");
+
+		GameObject obj = GameObject.Find("Sphere");
+		Debug.Log(obj.GetComponent<Renderer>().material.color);
+		
+		obj.GetComponent<Renderer>().material.color = Color.red;
+		Debug.Log(obj.GetComponent<Renderer>().material.color);
+	}
+
+}
