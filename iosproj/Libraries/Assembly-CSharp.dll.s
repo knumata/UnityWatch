fa7e9269c638e6d1acc711105589b468991a7acd
沼田 和幸 <kznumata@gmail.com>
2015-05-07 03:40:25 +0900

    add iosproj

---
+++ iosproj/Libraries/Assembly-CSharp.dll.s
@@ -0,0 +1,705 @@
+#if defined(__arm__)
+.text
+	.align 3
+methods:
+	.space 16
+	.align 2
+Lm_0:
+m_ButtonController__ctor:
+_m_0:
+
+	.byte 13,192,160,225,128,64,45,233,13,112,160,225,0,89,45,233,8,208,77,226,13,176,160,225,0,0,139,229,0,0,155,229
+bl p_1
+
+	.byte 8,208,139,226,0,9,189,232,8,112,157,229,0,160,157,232
+
+Lme_0:
+	.align 2
+Lm_1:
+m_ButtonController_Start:
+_m_1:
+
+	.byte 13,192,160,225,128,64,45,233,13,112,160,225,0,89,45,233,8,208,77,226,13,176,160,225,0,0,139,229,8,208,139,226
+	.byte 0,9,189,232,8,112,157,229,0,160,157,232
+
+Lme_1:
+	.align 2
+Lm_2:
+m_ButtonController_Update:
+_m_2:
+
+	.byte 13,192,160,225,128,64,45,233,13,112,160,225,0,89,45,233,8,208,77,226,13,176,160,225,0,0,139,229,8,208,139,226
+	.byte 0,9,189,232,8,112,157,229,0,160,157,232
+
+Lme_2:
+	.align 2
+Lm_3:
+m_ButtonController_changeBallColor:
+_m_3:
+
+	.byte 13,192,160,225,128,64,45,233,13,112,160,225,0,89,45,233,80,208,77,226,13,176,160,225,56,0,139,229,0,0,159,229
+	.byte 0,0,0,234
+	.long mono_aot_Assembly_CSharp_got - . -4
+	.byte 0,0,159,231
+bl p_2
+
+	.byte 0,0,159,229,0,0,0,234
+	.long mono_aot_Assembly_CSharp_got - .
+	.byte 0,0,159,231
+bl p_3
+
+	.byte 76,0,139,229,0,16,159,229,0,0,0,234
+	.long mono_aot_Assembly_CSharp_got - . + 4
+	.byte 1,16,159,231,0,224,144,229,1,128,160,225
+bl p_4
+
+	.byte 0,16,160,225,0,224,145,229
+bl p_5
+
+	.byte 0,32,160,225,8,0,139,226,2,16,160,225,0,224,146,229
+bl p_6
+
+	.byte 0,0,159,229,0,0,0,234
+	.long mono_aot_Assembly_CSharp_got - . + 8
+	.byte 0,0,159,231
+bl p_7
+
+	.byte 8,16,128,226,8,32,155,229,0,32,129,229,12,32,155,229,4,32,129,229,16,32,155,229,8,32,129,229,20,32,155,229
+	.byte 12,32,129,229
+bl p_2
+
+	.byte 76,0,155,229,0,16,159,229,0,0,0,234
+	.long mono_aot_Assembly_CSharp_got - . + 4
+	.byte 1,16,159,231,0,224,144,229,1,128,160,225,64,0,139,229
+bl p_4
+
+	.byte 0,16,160,225,0,224,145,229
+bl p_5
+
+	.byte 72,0,139,229,24,0,139,226
+bl p_8
+
+	.byte 72,192,155,229,12,0,160,225,68,0,139,229,24,16,155,229,28,32,155,229,32,48,155,229,36,0,155,229,0,0,141,229
+	.byte 68,0,155,229,0,224,156,229
+bl p_9
+
+	.byte 64,0,155,229,0,16,159,229,0,0,0,234
+	.long mono_aot_Assembly_CSharp_got - . + 4
+	.byte 1,16,159,231,0,224,144,229,1,128,160,225
+bl p_4
+
+	.byte 0,16,160,225,0,224,145,229
+bl p_5
+
+	.byte 0,32,160,225,40,0,139,226,2,16,160,225,0,224,146,229
+bl p_6
+
+	.byte 0,0,159,229,0,0,0,234
+	.long mono_aot_Assembly_CSharp_got - . + 8
+	.byte 0,0,159,231
+bl p_7
+
+	.byte 8,16,128,226,40,32,155,229,0,32,129,229,44,32,155,229,4,32,129,229,48,32,155,229,8,32,129,229,52,32,155,229
+	.byte 12,32,129,229
+bl p_2
+
+	.byte 80,208,139,226,0,9,189,232,8,112,157,229,0,160,157,232
+
+Lme_3:
+	.align 2
+Lm_5:
+m_wrapper_managed_to_native_System_Array_GetGenericValueImpl_int_object_:
+_m_5:
+
+	.byte 13,192,160,225,240,95,45,233,120,208,77,226,13,176,160,225,0,0,139,229,4,16,139,229,8,32,139,229
+bl p_10
+
+	.byte 16,16,141,226,4,0,129,229,0,32,144,229,0,32,129,229,0,16,128,229,16,208,129,229,15,32,160,225,20,32,129,229
+	.byte 0,0,155,229,0,0,80,227,16,0,0,10,0,0,155,229,4,16,155,229,8,32,155,229
+bl p_11
+
+	.byte 0,0,159,229,0,0,0,234
+	.long mono_aot_Assembly_CSharp_got - . + 12
+	.byte 0,0,159,231,0,0,144,229,0,0,80,227,10,0,0,26,16,32,139,226,0,192,146,229,4,224,146,229,0,192,142,229
+	.byte 104,208,130,226,240,175,157,232,150,0,160,227,6,12,128,226,2,4,128,226
+bl p_12
+bl p_13
+bl p_14
+
+	.byte 242,255,255,234
+
+Lme_5:
+.text
+	.align 3
+methods_end:
+.data
+	.align 3
+method_addresses:
+	.align 2
+	.long _m_0
+	.align 2
+	.long _m_1
+	.align 2
+	.long _m_2
+	.align 2
+	.long _m_3
+	.align 2
+	.long 0
+	.align 2
+	.long _m_5
+.text
+	.align 3
+method_offsets:
+
+	.long Lm_0 - methods,Lm_1 - methods,Lm_2 - methods,Lm_3 - methods,-1,Lm_5 - methods
+
+.text
+	.align 3
+method_info:
+mi:
+Lm_0_p:
+
+	.byte 0,0
+Lm_1_p:
+
+	.byte 0,0
+Lm_2_p:
+
+	.byte 0,0
+Lm_3_p:
+
+	.byte 0,7,2,3,4,5,4,4,5
+Lm_5_p:
+
+	.byte 0,1,6
+.text
+	.align 3
+method_info_offsets:
+
+	.long Lm_0_p - mi,Lm_1_p - mi,Lm_2_p - mi,Lm_3_p - mi,0,Lm_5_p - mi
+
+.text
+	.align 3
+extra_method_info:
+
+	.byte 0,1,6,83,121,115,116,101,109,46,65,114,114,97,121,58,71,101,116,71,101,110,101,114,105,99,86,97,108,117,101,73
+	.byte 109,112,108,32,40,105,110,116,44,111,98,106,101,99,116,38,41,0
+
+.text
+	.align 3
+extra_method_table:
+
+	.long 11,0,0,0,1,5,0,0
+	.long 0,0,0,0,0,0,0,0
+	.long 0,0,0,0,0,0,0,0
+	.long 0,0,0,0,0,0,0,0
+	.long 0,0
+.text
+	.align 3
+extra_method_info_offsets:
+
+	.long 1,5,1
+.text
+	.align 3
+method_order:
+
+	.long 0,16777215,0,1,2,3,5
+
+.text
+method_order_end:
+.text
+	.align 3
+class_name_table:
+
+	.short 11, 1, 0, 0, 0, 0, 0, 0
+	.short 0, 0, 0, 0, 0, 0, 0, 2
+	.short 0, 0, 0, 0, 0, 0, 0
+.text
+	.align 3
+got_info:
+
+	.byte 12,0,39,17,0,1,17,0,19,34,255,255,0,0,0,0,255,43,0,0,1,14,128,134,1,33,3,193,0,13,171,3
+	.byte 193,0,13,120,3,193,0,14,240,3,255,255,0,0,0,0,255,43,0,0,1,3,193,0,1,229,3,193,0,11,28,7
+	.byte 27,109,111,110,111,95,111,98,106,101,99,116,95,110,101,119,95,112,116,114,102,114,101,101,95,98,111,120,0,3,193,0
+	.byte 8,55,3,193,0,11,29,7,17,109,111,110,111,95,103,101,116,95,108,109,102,95,97,100,100,114,0,31,255,254,0,0
+	.byte 0,41,2,2,198,0,4,3,0,1,1,2,2,7,30,109,111,110,111,95,99,114,101,97,116,101,95,99,111,114,108,105
+	.byte 98,95,101,120,99,101,112,116,105,111,110,95,48,0,7,25,109,111,110,111,95,97,114,99,104,95,116,104,114,111,119,95
+	.byte 101,120,99,101,112,116,105,111,110,0,7,35,109,111,110,111,95,116,104,114,101,97,100,95,105,110,116,101,114,114,117,112
+	.byte 116,105,111,110,95,99,104,101,99,107,112,111,105,110,116,0
+.text
+	.align 3
+got_info_offsets:
+
+	.long 0,2,3,6,9,21,25
+.text
+	.align 3
+ex_info:
+ex:
+Le_0_p:
+
+	.byte 52,2,0,0
+Le_1_p:
+
+	.byte 44,2,0,0
+Le_2_p:
+
+	.byte 44,2,0,0
+Le_3_p:
+
+	.byte 129,180,2,26,0
+Le_5_p:
+
+	.byte 128,172,2,52,0
+.text
+	.align 3
+ex_info_offsets:
+
+	.long Le_0_p - ex,Le_1_p - ex,Le_2_p - ex,Le_3_p - ex,0,Le_5_p - ex
+
+.text
+	.align 3
+unwind_info:
+
+	.byte 25,12,13,0,76,14,8,135,2,68,14,24,136,6,139,5,140,4,142,3,68,14,32,68,13,11,25,12,13,0,76,14
+	.byte 8,135,2,68,14,24,136,6,139,5,140,4,142,3,68,14,104,68,13,11,33,12,13,0,72,14,40,132,10,133,9,134
+	.byte 8,135,7,136,6,137,5,138,4,139,3,140,2,142,1,68,14,160,1,68,13,11
+.text
+	.align 3
+class_info:
+LK_I_0:
+
+	.byte 0,128,144,8,0,0,1
+LK_I_1:
+
+	.byte 4,128,144,16,0,0,4,193,0,14,68,193,0,14,71,194,0,0,4,193,0,14,70
+.text
+	.align 3
+class_info_offsets:
+
+	.long LK_I_0 - class_info,LK_I_1 - class_info
+
+
+.text
+	.align 4
+plt:
+mono_aot_Assembly_CSharp_plt:
+
+	.byte 0,192,159,229,12,240,159,231
+	.long mono_aot_Assembly_CSharp_got - . + 24,0
+p_1:
+plt_UnityEngine_MonoBehaviour__ctor:
+
+	.byte 0,192,159,229,12,240,159,231
+	.long mono_aot_Assembly_CSharp_got - . + 28,26
+p_2:
+plt_UnityEngine_Debug_Log_object:
+
+	.byte 0,192,159,229,12,240,159,231
+	.long mono_aot_Assembly_CSharp_got - . + 32,31
+p_3:
+plt_UnityEngine_GameObject_Find_string:
+
+	.byte 0,192,159,229,12,240,159,231
+	.long mono_aot_Assembly_CSharp_got - . + 36,36
+p_4:
+plt_UnityEngine_GameObject_GetComponent_UnityEngine_Renderer:
+
+	.byte 0,192,159,229,12,240,159,231
+	.long mono_aot_Assembly_CSharp_got - . + 40,41
+p_5:
+plt_UnityEngine_Renderer_get_material:
+
+	.byte 0,192,159,229,12,240,159,231
+	.long mono_aot_Assembly_CSharp_got - . + 44,53
+p_6:
+plt_UnityEngine_Material_get_color:
+
+	.byte 0,192,159,229,12,240,159,231
+	.long mono_aot_Assembly_CSharp_got - . + 48,58
+p_7:
+plt__jit_icall_mono_object_new_ptrfree_box:
+
+	.byte 0,192,159,229,12,240,159,231
+	.long mono_aot_Assembly_CSharp_got - . + 52,63
+p_8:
+plt_UnityEngine_Color_get_red:
+
+	.byte 0,192,159,229,12,240,159,231
+	.long mono_aot_Assembly_CSharp_got - . + 56,93
+p_9:
+plt_UnityEngine_Material_set_color_UnityEngine_Color:
+
+	.byte 0,192,159,229,12,240,159,231
+	.long mono_aot_Assembly_CSharp_got - . + 60,98
+p_10:
+plt__jit_icall_mono_get_lmf_addr:
+
+	.byte 0,192,159,229,12,240,159,231
+	.long mono_aot_Assembly_CSharp_got - . + 64,103
+p_11:
+plt__icall_native_System_Array_GetGenericValueImpl_object_int_object_:
+
+	.byte 0,192,159,229,12,240,159,231
+	.long mono_aot_Assembly_CSharp_got - . + 68,123
+p_12:
+plt__jit_icall_mono_create_corlib_exception_0:
+
+	.byte 0,192,159,229,12,240,159,231
+	.long mono_aot_Assembly_CSharp_got - . + 72,141
+p_13:
+plt__jit_icall_mono_arch_throw_exception:
+
+	.byte 0,192,159,229,12,240,159,231
+	.long mono_aot_Assembly_CSharp_got - . + 76,174
+p_14:
+plt__jit_icall_mono_thread_interruption_checkpoint:
+
+	.byte 0,192,159,229,12,240,159,231
+	.long mono_aot_Assembly_CSharp_got - . + 80,202
+plt_end:
+.text
+	.align 3
+mono_image_table:
+
+	.long 3
+	.asciz "Assembly-CSharp"
+	.asciz "FE3AF06F-911F-4371-8400-3AEE29CFC51A"
+	.asciz ""
+	.asciz ""
+	.align 3
+
+	.long 0,0,0,0,0
+	.asciz "UnityEngine"
+	.asciz "E01BDA62-BD83-414F-81B2-CFC96D8FDFCF"
+	.asciz ""
+	.asciz ""
+	.align 3
+
+	.long 0,0,0,0,0
+	.asciz "mscorlib"
+	.asciz "C38A5D13-9B46-4C3E-BF1A-F028E21823D0"
+	.asciz ""
+	.asciz "7cec85d7bea7798e"
+	.align 3
+
+	.long 1,2,0,5,0
+.data
+	.align 3
+mono_aot_Assembly_CSharp_got:
+	.space 88
+got_end:
+.data
+	.align 3
+mono_aot_got_addr:
+	.align 2
+	.long mono_aot_Assembly_CSharp_got
+.data
+	.align 3
+mono_aot_file_info:
+
+	.long 7,88,15,6,1024,1024,128,0
+	.long 0,0,0,0,0
+.text
+	.align 2
+mono_assembly_guid:
+	.asciz "FE3AF06F-911F-4371-8400-3AEE29CFC51A"
+.text
+	.align 2
+mono_aot_version:
+	.asciz "66"
+.text
+	.align 2
+mono_aot_opt_flags:
+	.asciz "55650815"
+.text
+	.align 2
+mono_aot_full_aot:
+	.asciz "TRUE"
+.text
+	.align 2
+mono_runtime_version:
+	.asciz ""
+.text
+	.align 2
+mono_aot_assembly_name:
+	.asciz "Assembly-CSharp"
+.text
+	.align 3
+Lglobals_hash:
+
+	.short 73, 27, 0, 0, 0, 0, 0, 0
+	.short 0, 15, 0, 19, 0, 0, 0, 0
+	.short 0, 6, 0, 0, 0, 3, 0, 0
+	.short 0, 0, 0, 0, 0, 0, 0, 29
+	.short 0, 13, 0, 5, 0, 0, 0, 0
+	.short 0, 4, 0, 28, 0, 0, 0, 9
+	.short 0, 0, 0, 0, 0, 0, 0, 14
+	.short 0, 1, 0, 0, 0, 0, 0, 12
+	.short 74, 0, 0, 0, 0, 0, 0, 30
+	.short 0, 2, 75, 0, 0, 0, 0, 0
+	.short 0, 0, 0, 0, 0, 0, 0, 0
+	.short 0, 22, 0, 0, 0, 0, 0, 0
+	.short 0, 11, 0, 17, 0, 8, 0, 0
+	.short 0, 0, 0, 0, 0, 0, 0, 0
+	.short 0, 0, 0, 0, 0, 0, 0, 0
+	.short 0, 0, 0, 0, 0, 16, 0, 20
+	.short 0, 7, 73, 24, 0, 10, 0, 0
+	.short 0, 0, 0, 0, 0, 0, 0, 0
+	.short 0, 21, 0, 18, 76, 23, 0, 25
+	.short 0, 26, 0
+.text
+	.align 2
+name_0:
+	.asciz "methods"
+.text
+	.align 2
+name_1:
+	.asciz "methods_end"
+.text
+	.align 2
+name_2:
+	.asciz "method_addresses"
+.text
+	.align 2
+name_3:
+	.asciz "method_offsets"
+.text
+	.align 2
+name_4:
+	.asciz "method_info"
+.text
+	.align 2
+name_5:
+	.asciz "method_info_offsets"
+.text
+	.align 2
+name_6:
+	.asciz "extra_method_info"
+.text
+	.align 2
+name_7:
+	.asciz "extra_method_table"
+.text
+	.align 2
+name_8:
+	.asciz "extra_method_info_offsets"
+.text
+	.align 2
+name_9:
+	.asciz "method_order"
+.text
+	.align 2
+name_10:
+	.asciz "method_order_end"
+.text
+	.align 2
+name_11:
+	.asciz "class_name_table"
+.text
+	.align 2
+name_12:
+	.asciz "got_info"
+.text
+	.align 2
+name_13:
+	.asciz "got_info_offsets"
+.text
+	.align 2
+name_14:
+	.asciz "ex_info"
+.text
+	.align 2
+name_15:
+	.asciz "ex_info_offsets"
+.text
+	.align 2
+name_16:
+	.asciz "unwind_info"
+.text
+	.align 2
+name_17:
+	.asciz "class_info"
+.text
+	.align 2
+name_18:
+	.asciz "class_info_offsets"
+.text
+	.align 2
+name_19:
+	.asciz "plt"
+.text
+	.align 2
+name_20:
+	.asciz "plt_end"
+.text
+	.align 2
+name_21:
+	.asciz "mono_image_table"
+.text
+	.align 2
+name_22:
+	.asciz "mono_aot_got_addr"
+.text
+	.align 2
+name_23:
+	.asciz "mono_aot_file_info"
+.text
+	.align 2
+name_24:
+	.asciz "mono_assembly_guid"
+.text
+	.align 2
+name_25:
+	.asciz "mono_aot_version"
+.text
+	.align 2
+name_26:
+	.asciz "mono_aot_opt_flags"
+.text
+	.align 2
+name_27:
+	.asciz "mono_aot_full_aot"
+.text
+	.align 2
+name_28:
+	.asciz "mono_runtime_version"
+.text
+	.align 2
+name_29:
+	.asciz "mono_aot_assembly_name"
+.data
+	.align 3
+Lglobals:
+	.align 2
+	.long Lglobals_hash
+	.align 2
+	.long name_0
+	.align 2
+	.long methods
+	.align 2
+	.long name_1
+	.align 2
+	.long methods_end
+	.align 2
+	.long name_2
+	.align 2
+	.long method_addresses
+	.align 2
+	.long name_3
+	.align 2
+	.long method_offsets
+	.align 2
+	.long name_4
+	.align 2
+	.long method_info
+	.align 2
+	.long name_5
+	.align 2
+	.long method_info_offsets
+	.align 2
+	.long name_6
+	.align 2
+	.long extra_method_info
+	.align 2
+	.long name_7
+	.align 2
+	.long extra_method_table
+	.align 2
+	.long name_8
+	.align 2
+	.long extra_method_info_offsets
+	.align 2
+	.long name_9
+	.align 2
+	.long method_order
+	.align 2
+	.long name_10
+	.align 2
+	.long method_order_end
+	.align 2
+	.long name_11
+	.align 2
+	.long class_name_table
+	.align 2
+	.long name_12
+	.align 2
+	.long got_info
+	.align 2
+	.long name_13
+	.align 2
+	.long got_info_offsets
+	.align 2
+	.long name_14
+	.align 2
+	.long ex_info
+	.align 2
+	.long name_15
+	.align 2
+	.long ex_info_offsets
+	.align 2
+	.long name_16
+	.align 2
+	.long unwind_info
+	.align 2
+	.long name_17
+	.align 2
+	.long class_info
+	.align 2
+	.long name_18
+	.align 2
+	.long class_info_offsets
+	.align 2
+	.long name_19
+	.align 2
+	.long plt
+	.align 2
+	.long name_20
+	.align 2
+	.long plt_end
+	.align 2
+	.long name_21
+	.align 2
+	.long mono_image_table
+	.align 2
+	.long name_22
+	.align 2
+	.long mono_aot_got_addr
+	.align 2
+	.long name_23
+	.align 2
+	.long mono_aot_file_info
+	.align 2
+	.long name_24
+	.align 2
+	.long mono_assembly_guid
+	.align 2
+	.long name_25
+	.align 2
+	.long mono_aot_version
+	.align 2
+	.long name_26
+	.align 2
+	.long mono_aot_opt_flags
+	.align 2
+	.long name_27
+	.align 2
+	.long mono_aot_full_aot
+	.align 2
+	.long name_28
+	.align 2
+	.long mono_runtime_version
+	.align 2
+	.long name_29
+	.align 2
+	.long mono_aot_assembly_name
+
+	.long 0,0
+	.globl _mono_aot_module_Assembly_CSharp_info
+	.align 3
+_mono_aot_module_Assembly_CSharp_info:
+	.align 2
+	.long Lglobals
+.text
+	.align 3
+mem_end:
+#endif
